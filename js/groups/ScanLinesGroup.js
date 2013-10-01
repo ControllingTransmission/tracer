949969f722b859848e40dc3b7ba27fb4a09382c1
Dean Putney <mustardhamsters@gmail.com>
2013-09-30 19:14:34 -0700

    Adding ScanLinesGroup

---
+++ js/groups/ScanLinesGroup.js
@@ -0,0 +1,30 @@
+
+ScanLinesGroup = Group.clone().newSlots({
+	protoType: "SquaresGroup",
+	items: null,
+	spacing: 27,
+	itemXScale: 1000,
+	itemYScale: .05,
+	max: 50
+}).setSlots({
+	init: function()
+	{
+		Group.init.apply(this)
+		this.addMover(MaxMover.clone())
+		this.addSquares()
+	},
+	
+	addSquares: function()
+	{
+		var max = this._max
+		for (var y = -max; y < max-1; y ++)
+		{
+			var s = Square.clone()
+			s._object.scale.x = this._itemXScale
+			s._object.scale.y = this._itemYScale
+			s._object.position.y += y*this._spacing
+			this.addItem(s)
+		}
+	}
+})
+
